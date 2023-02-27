438fef9b352af195aa6030c232aa2d514630a773
a1excoder <aexnogreen@gmail.com>
2023-02-28 00:03:55 +0200

    created struct ErrorReturned and RequestParam for error processing

--- Types.go
+++ Types.go
@@ -5,3 +5,14 @@ type SuccessToken struct {
 	ExpiresIn   int    `json:"expires_in"`
 	UserId      int    `json:"user_id"`
 }
+
+type ErrorReturned struct {
+	ErrorCode    int                 `json:"error_code"`
+	ErrorMsg     string              `json:"error_msg"`
+	RequestParam []RequestParam[any] `json:"request_param"`
+}
+
+type RequestParam[T any] struct {
+	Key   string `json:"key"`
+	Value T      `json:"value"`
+}
