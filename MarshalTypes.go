9e10ac6d035faa972921d32aab68b2d108ad71df
a1excoder <aexnogreen@gmail.com>
2023-02-28 17:10:47 +0200

    created isError function

--- MarshalTypes.go
+++ MarshalTypes.go
@@ -1,1 +1,17 @@
 package gOvkApi
+
+import "encoding/json"
+
+func isError(data []byte) (bool, error) {
+	value := make(map[string]any)
+
+	if err := json.Unmarshal(data, &value); err != nil {
+		return false, err
+	}
+
+	if _, found := value["error_code"]; found {
+		return true, nil
+	}
+
+	return false, nil
+}
