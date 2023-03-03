6889e94736bea06c1634b4b7970cb9000f4c2751
a1excoder <aexnogreen@gmail.com>
2023-03-03 16:03:53 +0200

    optimized isError function

--- MarshalTypes.go
+++ MarshalTypes.go
@@ -3,19 +3,13 @@ package gOvkApi
 import "encoding/json"
 
 func isError(data []byte) (*ErrorReturned, bool, error) {
-	value := make(map[string]any)
-
-	if err := json.Unmarshal(data, &value); err != nil {
+	errPtr, err := unmarshalError(data)
+	if err != nil {
 		return nil, false, err
 	}
 
-	if _, found := value["error_code"]; found {
-		ovkErr, err := unmarshalError(data)
-		if err != nil {
-			return nil, true, err
-		}
-
-		return ovkErr, true, nil
+	if errPtr.ErrorMsg != "" {
+		return errPtr, true, nil
 	}
 
 	return nil, false, nil
