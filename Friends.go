34497f4a1e532aae603d721319a7256bef8dff60
a1excoder <aexnogreen@gmail.com>
2023-03-03 17:42:08 +0200

    created getFriends for getting list of user friends

--- Friends.go
+++ Friends.go
@@ -1,1 +1,35 @@
 package gOvkApi
+
+import (
+	"net/url"
+	"strconv"
+)
+
+func (authData *AuthData) getFriends(userId, count, offset int) (*GetFriends, *ErrorReturned, error) {
+	params := url.Values{}
+	params.Add("access_token", authData.Token.AccessToken)
+	params.Add("user_id", strconv.Itoa(userId))
+	params.Add("count", strconv.Itoa(count))
+	params.Add("offset", strconv.Itoa(offset))
+
+	body, _, err := makeRequest(authData.Instance+"/method/Friends.get", params, MethodGet)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ovkErr, isErr, err := isError(body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if isErr {
+		return nil, ovkErr, nil
+	}
+
+	profileInfo, err := unmarshalAny[GetFriends](body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return profileInfo, nil, nil
+}
