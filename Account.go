7ed25f1d4c522b8f2fbe5cbcff25b1d03d26c28d
a1excoder <aexnogreen@gmail.com>
2023-03-03 16:50:07 +0200

    created getCounters method for getting counters

--- Account.go
+++ Account.go
@@ -53,3 +53,29 @@ func (authData *AuthData) getInfo() (*Info, *ErrorReturned, error) {
 
 	return info, nil, nil
 }
+
+func (authData *AuthData) getCounters() (*Counters, *ErrorReturned, error) {
+	params := url.Values{}
+	params.Add("access_token", authData.Token.AccessToken)
+
+	body, _, err := makeRequest(authData.Instance+"/method/Account.getCounters", params, MethodGet)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ovkErr, isErr, err := isError(body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if isErr {
+		return nil, ovkErr, nil
+	}
+
+	counters, err := unmarshalAny[Counters](body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return counters, nil, nil
+}
