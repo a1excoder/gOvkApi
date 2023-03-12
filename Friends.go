213e6b8268814dc02f2f632dac7c9ca0379e2b3b
a1excoder <aexnogreen@gmail.com>
2023-03-12 03:10:12 +0200

    update Friends methods for new ErrorReturned

--- Friends.go
+++ Friends.go
@@ -5,141 +5,141 @@ import (
 	"strconv"
 )
 
-func (authData *AuthData) GetFriends(userId, count, offset int) (*GetFriends, *ErrorReturned, error) {
+func (authData *AuthData) GetFriends(userId, count, offset int) (*GetFriends, ErrorReturned) {
 	params := url.Values{}
 	params.Add("access_token", authData.Token.AccessToken)
 	params.Add("user_id", strconv.Itoa(userId))
 	params.Add("count", strconv.Itoa(count))
 	params.Add("offset", strconv.Itoa(offset))
 
+	errRet := ErrorReturned{}
+
 	body, _, err := makeRequest(authData.Instance+"/method/Friends.get", params, methodGet)
 	if err != nil {
-		return nil, nil, err
+		errRet.Err = err
+		return nil, errRet
 	}
 
-	ovkErr, err := isError(body)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	if ovkErr != nil {
-		return nil, ovkErr, nil
+	errRet.OvkError, errRet.Err = isError(body)
+	if errRet.Err != nil || errRet.OvkError != nil {
+		return nil, errRet
 	}
 
 	getFriends, err := unmarshalAny[GetFriends](body)
 	if err != nil {
-		return nil, nil, err
+		errRet.Err = err
+		return nil, errRet
 	}
 
-	return getFriends, nil, nil
+	return getFriends, errRet
 }
 
-func (authData *AuthData) AddFriend(userId int) (*ResponseFriends, *ErrorReturned, error) {
+func (authData *AuthData) AddFriend(userId int) (*ResponseFriends, ErrorReturned) {
 	params := url.Values{}
 	params.Add("access_token", authData.Token.AccessToken)
 	params.Add("user_id", strconv.Itoa(userId))
+
+	errRet := ErrorReturned{}
 
 	body, _, err := makeRequest(authData.Instance+"/method/Friends.add", params, methodGet)
 	if err != nil {
-		return nil, nil, err
+		errRet.Err = err
+		return nil, errRet
 	}
 
-	ovkErr, err := isError(body)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	if ovkErr != nil {
-		return nil, ovkErr, nil
+	errRet.OvkError, errRet.Err = isError(body)
+	if errRet.Err != nil || errRet.OvkError != nil {
+		return nil, errRet
 	}
 
 	respFriends, err := unmarshalAny[ResponseFriends](body)
 	if err != nil {
-		return nil, nil, err
+		errRet.Err = err
+		return nil, errRet
 	}
 
-	return respFriends, nil, nil
+	return respFriends, errRet
 }
 
-func (authData *AuthData) DeleteFriend(userId int) (*ResponseFriends, *ErrorReturned, error) {
+func (authData *AuthData) DeleteFriend(userId int) (*ResponseFriends, ErrorReturned) {
 	params := url.Values{}
 	params.Add("access_token", authData.Token.AccessToken)
 	params.Add("user_id", strconv.Itoa(userId))
 
+	errRet := ErrorReturned{}
+
 	body, _, err := makeRequest(authData.Instance+"/method/Friends.delete", params, methodGet)
 	if err != nil {
-		return nil, nil, err
+		errRet.Err = err
+		return nil, errRet
 	}
 
-	ovkErr, err := isError(body)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	if ovkErr != nil {
-		return nil, ovkErr, nil
+	errRet.OvkError, errRet.Err = isError(body)
+	if errRet.Err != nil || errRet.OvkError != nil {
+		return nil, errRet
 	}
 
 	respFriends, err := unmarshalAny[ResponseFriends](body)
 	if err != nil {
-		return nil, nil, err
+		errRet.Err = err
+		return nil, errRet
 	}
 
-	return respFriends, nil, nil
+	return respFriends, errRet
 }
 
-func (authData *AuthData) GetRequests(extended, count, offset int) (*GetRequests, *ErrorReturned, error) {
+func (authData *AuthData) GetRequests(extended, count, offset int) (*GetRequests, ErrorReturned) {
 	params := url.Values{}
 	params.Add("access_token", authData.Token.AccessToken)
 	params.Add("extended", strconv.Itoa(extended))
 	params.Add("count", strconv.Itoa(count))
 	params.Add("offset", strconv.Itoa(offset))
 
+	errRet := ErrorReturned{}
+
 	body, _, err := makeRequest(authData.Instance+"/method/Friends.getRequests", params, methodGet)
 	if err != nil {
-		return nil, nil, err
+		errRet.Err = err
+		return nil, errRet
 	}
 
-	ovkErr, err := isError(body)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	if ovkErr != nil {
-		return nil, ovkErr, nil
+	errRet.OvkError, errRet.Err = isError(body)
+	if errRet.Err != nil || errRet.OvkError != nil {
+		return nil, errRet
 	}
 
 	getReqFriends, err := unmarshalAny[GetRequests](body)
 	if err != nil {
-		return nil, nil, err
+		errRet.Err = err
+		return nil, errRet
 	}
 
-	return getReqFriends, nil, nil
+	return getReqFriends, errRet
 }
 
-func (authData *AuthData) AreFriends(userIds string) (*AreFriends, *ErrorReturned, error) {
+func (authData *AuthData) AreFriends(userIds string) (*AreFriends, ErrorReturned) {
 	params := url.Values{}
 	params.Add("access_token", authData.Token.AccessToken)
 	params.Add("user_ids", userIds)
 
+	errRet := ErrorReturned{}
+
 	body, _, err := makeRequest(authData.Instance+"/method/Friends.areFriends", params, methodGet)
 	if err != nil {
-		return nil, nil, err
+		errRet.Err = err
+		return nil, errRet
 	}
 
-	ovkErr, err := isError(body)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	if ovkErr != nil {
-		return nil, ovkErr, nil
+	errRet.OvkError, errRet.Err = isError(body)
+	if errRet.Err != nil || errRet.OvkError != nil {
+		return nil, errRet
 	}
 
 	areFriendsReq, err := unmarshalAny[AreFriends](body)
 	if err != nil {
-		return nil, nil, err
+		errRet.Err = err
+		return nil, errRet
 	}
 
-	return areFriendsReq, nil, nil
+	return areFriendsReq, errRet
 }
