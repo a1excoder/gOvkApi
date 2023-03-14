c48fedcb6a3bffc9208f2019b81672e147a946a9
a1excoder <aexnogreen@gmail.com>
2023-03-15 00:01:44 +0200

    created GetGroups method

---
+++ Groups.go
@@ -0,0 +1,35 @@
+package gOvkApi
+
+import (
+	"net/url"
+	"strconv"
+)
+
+func (authData *AuthData) GetGroups(userId, count, offset int) (*GetGroups, ErrorReturned) {
+	params := url.Values{}
+	params.Add("access_token", authData.Token.AccessToken)
+	params.Add("user_id", strconv.Itoa(userId))
+	params.Add("count", strconv.Itoa(count))
+	params.Add("offset", strconv.Itoa(offset))
+
+	errRet := ErrorReturned{}
+
+	body, _, err := makeRequest(authData.Instance+"/method/Groups.get", params, methodGet)
+	if err != nil {
+		errRet.Err = err
+		return nil, errRet
+	}
+
+	errRet.OvkError, errRet.Err = isError(body)
+	if errRet.Err != nil || errRet.OvkError != nil {
+		return nil, errRet
+	}
+
+	getGroups, err := unmarshalAny[GetGroups](body)
+	if err != nil {
+		errRet.Err = err
+		return nil, errRet
+	}
+
+	return getGroups, errRet
+}
