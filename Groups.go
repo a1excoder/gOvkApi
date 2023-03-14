c94906eafee2241039b9cf8d033419eda384e41a
a1excoder <aexnogreen@gmail.com>
2023-03-15 01:36:22 +0200

    created GetById method for getting groups info by ids

--- Groups.go
+++ Groups.go
@@ -1,8 +1,10 @@
 package gOvkApi
 
 import (
+	"fmt"
 	"net/url"
 	"strconv"
+	"strings"
 )
 
 func (authData *AuthData) GetGroups(userId, count, offset int, fields string) (*GetGroups, ErrorReturned) {
@@ -33,4 +35,34 @@ func (authData *AuthData) GetGroups(userId, count, offset int, fields string) (*
 	}
 
 	return getGroups, errRet
+}
+
+func (authData *AuthData) GetById(groupIds []int, count, offset int, fields string) (*GetById, ErrorReturned) {
+	params := url.Values{}
+	params.Add("access_token", authData.Token.AccessToken)
+	params.Add("group_ids", strings.Trim(strings.Join(strings.Fields(fmt.Sprint(groupIds)), ","), "[]"))
+	params.Add("count", strconv.Itoa(count))
+	params.Add("offset", strconv.Itoa(offset))
+	params.Add("fields", fields)
+
+	errRet := ErrorReturned{}
+
+	body, _, err := makeRequest(authData.Instance+"/method/Groups.getById", params, methodGet)
+	if err != nil {
+		errRet.Err = err
+		return nil, errRet
+	}
+
+	errRet.OvkError, errRet.Err = isError(body)
+	if errRet.Err != nil || errRet.OvkError != nil {
+		return nil, errRet
+	}
+
+	getById, err := unmarshalAny[GetById](body)
+	if err != nil {
+		errRet.Err = err
+		return nil, errRet
+	}
+
+	return getById, errRet
 }
